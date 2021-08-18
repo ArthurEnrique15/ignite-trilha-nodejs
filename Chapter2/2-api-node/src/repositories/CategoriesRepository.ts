ccb130d8c3482ac3ee882f7001175ac1d62124b1
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-17 23:57:16 -0300

    [CHAPTER 2] criando o cadastro e listagem de categorias

---
+++ Chapter2/2-api-node/src/repositories/CategoriesRepository.ts
@@ -0,0 +1,35 @@
+import { Category } from "../model/Category";
+
+// DTO => Data transfer object
+// Criar um objeto responsável por transferir os dados entre uma camada e outra
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+class CategoriesRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ description, name }: ICreateCategoryDTO): void {
+        const category = new Category();
+
+        // faz a atribuição de todos os campos de category
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
+    }
+}
+
+export { CategoriesRepository };
---
+++ Chapter2/2-api-node/src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+
+const categoriesRepository = new CategoriesRepository();
+
+// cadastro de categoria
+categoriesRoutes.post("/", (request, response) => {
+    const { name, description } = request.body;
+
+    categoriesRepository.create({ name, description });
+
+    return response.status(201).send();
+});
+
+// listagem de categorias
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+
+    return response.json(all);
+});
+
+export { categoriesRoutes };
