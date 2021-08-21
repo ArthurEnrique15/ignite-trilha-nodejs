64975d928a84aa8ae47fa95c20f81949cc2592c6
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-21 18:35:47 -0300

    [CHAPTER 2] Criando repositório de especificação

--- Chapter2/2-api-node/src/modules/cars/services/CreateCategoryService.ts
+++ Chapter2/2-api-node/src/modules/cars/services/CreateCategoryService.ts
@@ -8,8 +8,11 @@ interface IRequest {
 // Classe com a responsabilidade única de criar uma categoria
 class CreateCategoryService {
     // Princípio de substituição de Liskov: utilizando a interface aqui, podemos passar qualquer classe que implementar essa interface, então ao criar uma categoria, não precisamos nos preocupar com como os dados são salvos, ou qual o tipo de banco de dados.
+
+    // O private é usado para ter acesso ao this
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
+    // desestruturação do IRequest
     execute({ description, name }: IRequest): void {
         const categoryAlreadyExists =
             this.categoriesRepository.findByName(name);
