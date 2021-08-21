431ecb20397c363e32239eac37f5ae8d660a2ba5
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-21 20:50:02 -0300

    [CHAPTER 2] Criando o use case para importar categorias utilizando arquivos

--- Chapter2/2-api-node/src/routes/categories.routes.ts
+++ Chapter2/2-api-node/src/routes/categories.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
+// dest = destino onde os arquivos importados serão salvos
 const upload = multer({
     dest: "./tmp",
 });
@@ -21,10 +23,10 @@ categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 });
 
+// passa o multer como um middleware para a aplicação
+// single = recebe apenas um arquivo por vez
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.send();
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
