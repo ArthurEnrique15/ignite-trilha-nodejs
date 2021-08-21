8d8aca90f53763620446f948edfb2d8dd46494ff
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-21 20:37:50 -0300

    [CHAPTER 2] Criando upload de arquivos

--- Chapter2/2-api-node/src/routes/categories.routes.ts
+++ Chapter2/2-api-node/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 // cadastro de categoria
 // rota recebe a requisição, chama o serviço, executa algo e da o retorno
@@ -14,6 +19,12 @@ categoriesRoutes.post("/", (request, response) => {
 // listagem de categorias
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
 });
 
 export { categoriesRoutes };
