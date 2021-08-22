600b73165e2faaaffe5d4b5b3e3573b99108727c
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-21 21:07:59 -0300

    [CHAPTER 2] Usando o conceito de stream - Realizar a leitura do arquivo em partes

--- Chapter2/2-api-node/src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ Chapter2/2-api-node/src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,21 @@
+// módulo nativo do node - file system
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        // cria um stream de leitura para o arquivo, passando o path dele
+        const stream = fs.createReadStream(file.path);
+
+        // lê linha por linha do arquivo
+        const parseFile = csvParse();
+
+        // pipe - pega o que foi lido no arquivo e joga para outro lugar
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
