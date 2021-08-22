a462a4b97295f9fbd0a54f8f7d2bdd889cdde739
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-08-21 22:58:26 -0300

    [CHAPTER 2] Iniciando a documentação com swagger e removendo os arquivos de upload

--- Chapter2/2-api-node/src/server.ts
+++ Chapter2/2-api-node/src/server.ts
@@ -1,10 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+// rota na qual a documentação será disponibilizada
+// setup -> arquivo json onde fica toda a documentação da api
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
