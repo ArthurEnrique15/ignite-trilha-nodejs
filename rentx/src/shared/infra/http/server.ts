d3381c43db956d61a64d352bfa3f92bab0963053
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-09-13 11:45:00 -0300

    [CHAPTER 4] Criando seed de usuário para cadastrar um usuário administrador

--- rentx/src/shared/infra/http/server.ts
+++ rentx/src/shared/infra/http/server.ts
@@ -4,12 +4,14 @@ import express, { NextFunction, Response, Request } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "@shared/infra/typeorm";
 import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import { router } from "@shared/infra/http/routes";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
+
+createConnection();
 
 const app = express();
 
--- rentx/src/shared/infra/typeorm/index.ts
+++ rentx/src/shared/infra/typeorm/index.ts
@@ -1,17 +1,22 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (host = "database"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+    return createConnection(Object.assign(defaultOptions, { host }));
+};
 
 // A propriedade host por padrão é uma propriedade somente de leitura.
 // Se tentarmos sobrescrever o valor dela, o typescript recusa.
 // Por esse motivo criamos uma interface, somente com a propriedade host e forçamos que o tipo nas nossas options seja da interface.
-interface IOptions {
-    host: string;
-}
+// interface IOptions {
+//     host: string;
+// }
 
-getConnectionOptions().then((options) => {
-    // atributos newOptions e options se comportam como se fossem o mesmo objeto e conseguimos sobrescrever o valor do host
-    const newOptions = options as IOptions;
-    newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-    createConnection({
-        ...options,
-    });
-});
+// getConnectionOptions().then((options) => {
+//     // atributos newOptions e options se comportam como se fossem o mesmo objeto e conseguimos sobrescrever o valor do host
+//     const newOptions = options as IOptions;
+//     newOptions.host = "database"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//     createConnection({
+//         ...options,
+//     });
+// });
