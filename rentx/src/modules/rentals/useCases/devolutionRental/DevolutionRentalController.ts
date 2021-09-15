b6510aecb32963f70a8a3b54bc87a98c024d4801
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-09-15 20:34:08 -0300

    [CHAPTER 5] Criando controller de devolução de carro

--- rentx/src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
+++ rentx/src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -1,5 +1,23 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { DevolutionRentalUseCase } from "./DevolutionRentalUseCase";
+
 class DevolutionRentalController {
-    async handle() {}
+    async handle(request: Request, response: Response) {
+        const { id: user_id } = request.user;
+        const { id } = request.params;
+        const devolutionRentalUseCase = container.resolve(
+            DevolutionRentalUseCase
+        );
+
+        const rental = await devolutionRentalUseCase.execute({
+            id,
+            user_id,
+        });
+
+        return response.status(200).json(rental);
+    }
 }
 
 export { DevolutionRentalController };
