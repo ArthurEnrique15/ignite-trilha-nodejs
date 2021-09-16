fa6a12707ef02c9562855a25084603700a4cd39a
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-09-16 19:40:27 -0300

    [CHAPTER 5] Criando repositório de refresh token

---
+++ rentx/src/modules/accounts/repositories/IUserTokensRepository.ts
@@ -0,0 +1,12 @@
+import { ICreateUserTokenDTO } from "../dtos/ICreateUserTokenDTO";
+import { UserTokens } from "../infra/typeorm/entities/UserTokens";
+
+interface IUserTokensRepository {
+    create({
+        refresh_token,
+        user_id,
+        expires_date,
+    }: ICreateUserTokenDTO): Promise<UserTokens>;
+}
+
+export { IUserTokensRepository };
