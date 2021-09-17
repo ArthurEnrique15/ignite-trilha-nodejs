7c3d9c3f81de11e6ed57c0fb9dc3656e8c50f7cc
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-09-16 21:13:54 -0300

    [CHAPTER 5] Refatorando autenticação de usuário

---
+++ rentx/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "7b6e492f1a222026f8467ebeeaa3e559",
+    expires_in_token: "15m",
+    secret_refresh_token: "ee57d7f624660da494d99587d970849c",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30,
+};
