83905d145c0c402d4abcdebd1c56e0f8945b78fc
ArthurEnrique15 <arthurenrique1512@gmail.com>
2021-09-17 16:01:37 -0300

    [CHAPTER 5] Finalizando a recuperação de senha com o caso de uso e controler de reset senha

---
+++ rentx/src/modules/accounts/useCases/resetPasswordUser/ResetUserPasswordController.ts
@@ -0,0 +1,24 @@
+import { Response, Request } from "express";
+import { container } from "tsyringe";
+
+import { ResetUserPasswordUseCase } from "./ResetUserPasswordUseCase";
+
+class ResetUserPasswordController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { token } = request.query;
+        const { password } = request.body;
+
+        const resetUserPasswordUseCase = container.resolve(
+            ResetUserPasswordUseCase
+        );
+
+        await resetUserPasswordUseCase.execute({
+            token: String(token),
+            password,
+        });
+
+        return response.status(201).send();
+    }
+}
+
+export { ResetUserPasswordController };
--- rentx/src/shared/infra/http/routes/password.routes.ts
+++ rentx/src/shared/infra/http/routes/password.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { ResetUserPasswordController } from "@modules/accounts/useCases/resetPasswordUser/ResetUserPasswordController";
 import { SendRecoverPasswordMailController } from "@modules/accounts/useCases/sendRecoverPasswordMail/SendRecoverPasswordMailController";
 
 const passwordRoutes = Router();
@@ -7,6 +8,10 @@ const passwordRoutes = Router();
 const sendRecoverPasswordMailController =
     new SendRecoverPasswordMailController();
 
+const resetUserPasswordController = new ResetUserPasswordController();
+
 passwordRoutes.post("/recover", sendRecoverPasswordMailController.handle);
+
+passwordRoutes.post("/reset", resetUserPasswordController.handle);
 
 export { passwordRoutes };
